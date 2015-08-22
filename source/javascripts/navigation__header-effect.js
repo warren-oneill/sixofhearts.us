23e1b8932b92dcdb6b80a611b9bcb4db092adc39
Paul S. Chun <paul@sixofhearts.us>
2015-08-22 16:27:56 +0200

    add logomark and heartbeat

---
+++ source/javascripts/navigation__header-effect.js
@@ -0,0 +1,25 @@
+//=require velocity/velocity.min.js
+
+const logomark = document.querySelector('.logomark');
+
+logomark.addEventListener('mouseenter', function() {
+  var pumping = setInterval(function(){ pumpHeart() }, 1000);
+  pumpHeart();
+  logomark.addEventListener('mouseout', function(e) {
+    clearInterval(pumping);
+  });
+});
+
+function pumpHeart() {
+  const vi = logomark.querySelector('.vi');
+  const heart = logomark.querySelector('.heart');
+
+  vi.className = 'vi pump';
+  setTimeout(function() {
+    heart.className = 'heart pump';
+    setTimeout(function() {
+      vi.className = 'vi';
+      heart.className = 'heart';
+    }, 300);
+  }, 100);
+};
